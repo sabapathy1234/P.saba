241e1935bdf21a82308d7d17921690be366f146c
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-06-26 23:55:35 -0700

    Update saba2.py

--- saba2.py
+++ saba2.py
@@ -1,5 +1,8 @@
-a=int(input(""));
-if (a&1==1):
-		print("odd")
+n6=int(input())
+if(n6>0):
+    if(n6%2==0):
+        print("Even")
+    else:
+        print("Odd")
 else:
-    	print("Even")
+    print("invalid")
