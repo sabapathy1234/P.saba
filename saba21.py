6d09d69cc78f276f35e0741dbddb8b595556cffd
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-06-27 23:58:54 -0700

    Update saba21.py

--- saba21.py
+++ saba21.py
@@ -1,3 +1,3 @@
-x1,y1,n1=map(int,input().split())
-if(x1>1 and x1<=100000):
-  print(int((x1/2)*(2*y+(x1-1)*n1)))
+x2,y2,n2=map(int,input().split())
+if(x2>1 and x2<=100000):
+  print(int((x2/2)*(2*y2+(x2-1)*n2)))
