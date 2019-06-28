33400ea7339d08ec03f59dce02eed5973dfd0b85
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-06-27 21:29:51 -0700

    Update saba5.py

--- saba5.py
+++ saba5.py
@@ -1,7 +1,7 @@
-in1,in2,in3= map(int,input().split())
-if(in1<=in2<=in3):
+in2,in3,in34= map(int,input().split())
+if(in2<=in3<=in4):
+    print(in4)
+elif(in2<=in4<=in3):
     print(in3)
-elif(in1<=in3<=in2):
-    print(in2)
 else:
-    print(in1)
+    print(in2)
