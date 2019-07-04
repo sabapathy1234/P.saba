16e16670258f1be375dc4f42e6ba9d12073331c9
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-07-03 23:44:43 -0700

    Update saba61.py

--- saba61.py
+++ saba61.py
@@ -1,6 +1,6 @@
-sab=input()
-for i in range(0,len(sab)):
-   if(sab[i].isalpha() and sab[i].isdigit()):
+sab23=input()
+for i in range(0,len(sab23)):
+   if(sab23[i].isalpha() and sab23[i].isdigit()):
     print("No")
 else:
   print("Yes")
