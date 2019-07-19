507ff59545960cf9178ab72d4744e62fa39b7228
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-07-19 13:54:20 +0530

    Create sab23.py

---
+++ sab23.py
@@ -0,0 +1,7 @@
+ss2=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+sa2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+c2=str(input())
+for d1 in range(20):
+  if(c2==ss2[d1]):
+    print(sa2[d1])
+    break
