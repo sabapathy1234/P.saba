5002eba702065fcd100a5f6c23dcec490c8046ea
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-07-19 17:19:49 +0530

    Update sab27.py

--- sab27.py
+++ sab27.py
@@ -1,6 +1,6 @@
-s1=input()
-a1=len(s1)
-b=[]
+v11=input()
+l11=len(v11)
+z=[]
 for x in range(0,l1,2):
-    b.append(s1[x:x+2][::-1])
-print(''.join(b))
+    z1.append(v11[x:x+2][::-1])
+print(''.join(z1))
