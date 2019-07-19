35eef8e4f01e0411b940d6ba69700ad2104168ae
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-07-19 11:44:05 +0530

    Update saba19.py

--- saba19.py
+++ saba19.py
@@ -1,7 +1,6 @@
-num11=int(input())
-f1=1
-for i in range(1,num11+1):
-    if(i>=0):
-        f1=f1*i
-        i+=1
-print(f1)
+sd2=int(input())
+fa=1
+for i in range(1,sd2+1):
+  if (i>=0):
+    fa=fa*i
+print(fa)
