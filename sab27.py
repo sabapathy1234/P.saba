70b079042783f9c0825cf4daa9c3ccfd3fc5d470
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-07-19 17:23:08 +0530

    Update sab27.py

--- sab27.py
+++ sab27.py
@@ -1,6 +1,6 @@
-v1=input()
-l1=len(v1)
-z=[]
-for x in range(0,l1,2):
-    z.append(v1[x:x+2][::-1])
-print(''.join(z))
+string1 = input()
+lengt1 = len(string1)
+mylist = []
+for i in range(0,lengt1,2):
+    mylist.append(string1[i:i+2][::-1])
+print("".join(mylist))
