a7dc2d9395bbdd440fc00819199cdc3f4d4b5e43
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-07-31 13:59:27 +0530

    Update ss3.py

--- ss3.py
+++ ss3.py
@@ -1,5 +1,6 @@
+from itertools import combinations
 N9,k9=map(int,input().split())
-a9=len(str(N9))
-lst=list(combinations(str(N9),a9-k9))
+a=len(str(N9))
+lst=list(combinations(str(N9),a-k9))
 lst=sorted(lst)
 print(*lst[0],sep='')
