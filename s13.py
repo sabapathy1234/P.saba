c83934da3c5a24ced0a1d25c9a472b9207c29bb3
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-08-05 14:36:03 +0530

    Create s13.py

---
+++ s13.py
@@ -0,0 +1,17 @@
+#s
+x9,y9=map(int,input().split())
+arr=list(map(int,input().split()))
+arr.sort(reverse=True)
+a=0
+total=y9
+for i in arr:
+    if total>=i:
+        rem=int(total/i)
+        a+=rem
+        total=total - (i*rem)
+    if total==0:
+        break
+if total==0:
+    print(a)
+else:
+    print("it's not possible to sum up coins in such a way that they um upto",y9)
