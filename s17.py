11ce0bf80128c526817f9487f3fa72c59dc21998
sabapathy1234 <52271158+sabapathy1234@users.noreply.github.com>
2019-08-14 15:40:49 +0530

    Create s17.py

---
+++ s17.py
@@ -0,0 +1,14 @@
+#s
+def compute_gcd(n1,n2):   #compute the gcd
+    while(n2):
+        n1,n2=n2,n1%n2      
+
+    return n1
+
+def compute_lcm(n1,n2):   
+    lcm=(n1*n2)//compute_gcd(n1,n2)   #compute the lcm
+    return lcm
+
+inp=list(map(int,input().split()))  
+print(compute_lcm(inp[0],inp[1]))
+© 2019 GitHub, Inc.
